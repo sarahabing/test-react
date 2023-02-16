384d4d472c638c61edef7f1b247d30c3f316b0a7
sarahabing <abingsa1@gmail.com>
2023-02-16 10:34:47 -0700

    ugh

--- react-demo/.eslintrc.cjs
+++ react-demo/.eslintrc.cjs
@@ -1,26 +1,26 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		'jest/globals': true,
-	},
-	extends: ['plugin:react/recommended', 'standard-with-typescript'],
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		project: ['tsconfig.json'],
-	},
-	plugins: ['react', 'jest'],
-	rules: {
-		'react/react-in-jsx-scope': 'off',
-		semi: 'off',
-		'@typescript-eslint/semi': ['error'],
-	},
-	root: true,
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    semi: 'off',
+    '@typescript-eslint/semi': ['error'],
+  },
+  root: true,
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
--- react-demo/vite.config.ts
+++ react-demo/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	root: '.',
-	build: {
-		outDir: './build',
-	},
+  plugins: [react()],
+  root: '.',
+  build: {
+    outDir: './build',
+  },
 })
