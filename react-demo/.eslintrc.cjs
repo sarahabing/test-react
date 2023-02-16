5ab808382ba4760a404e2c1eac5dc203cb8f18c1
sarahabing <abingsa1@gmail.com>
2023-02-16 10:31:48 -0700

    futsing with linters

--- react-demo/.eslintrc.cjs
+++ react-demo/.eslintrc.cjs
@@ -2,25 +2,25 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
-		"jest/globals": true,
+		'jest/globals': true,
 	},
-	extends: ["plugin:react/recommended", "standard-with-typescript"],
+	extends: ['plugin:react/recommended', 'standard-with-typescript'],
 	overrides: [],
 	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-		project: ["tsconfig.json"],
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['tsconfig.json'],
 	},
-	plugins: ["react", "jest"],
+	plugins: ['react', 'jest'],
 	rules: {
-		"react/react-in-jsx-scope": "off",
-		semi: "off",
-		"@typescript-eslint/semi": ["error"],
+		'react/react-in-jsx-scope': 'off',
+		semi: 'off',
+		'@typescript-eslint/semi': ['error'],
 	},
 	root: true,
 	settings: {
 		react: {
-			version: "detect",
+			version: 'detect',
 		},
 	},
-};
+}
