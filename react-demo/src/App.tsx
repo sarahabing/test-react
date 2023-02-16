7e1f498b2e44de12f2e63eccd8296915a698af34
sarahabing <abingsa1@gmail.com>
2023-02-16 13:16:56 -0700

    added imports

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,6 +1,8 @@
 import { theme } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import TopBar from './scenes/global/TopBar';
+import Dashboard from './scenes/dashboard';
+import SideBar from './scenes/global/SideBar';
 
 function App (): JSX.Element {
 
