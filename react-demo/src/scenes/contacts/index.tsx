fe301c2cce18b8527ed1ae92fc1e2d38eaef1410
sarahabing <abingsa1@gmail.com>
2023-02-16 17:31:13 -0700

    rest of tables

--- react-demo/src/scenes/contacts/index.tsx
+++ react-demo/src/scenes/contacts/index.tsx
@@ -1,6 +1,101 @@
+import { useTheme } from '@mui/material'
+import { Box } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+import { Header } from '../../components/Header'
+import { mockDataContacts} from '../../data/mockData'
+import { colors } from '../../theme'
+
 
 function Contacts(): JSX.Element {
-    return (<div>Contacts</div>)
+  const theme = useTheme()
+  const cols = [
+    { field: 'id', headerName: 'ID', flex: 0.5 },
+    { field: 'registrarId', headerName: 'Registrar ID' },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      cellClassName: 'name-column--cell'
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      headerAlign: 'left',
+      align: 'left'
+    },
+    {
+      field: 'phone',
+      headerName: 'Phone Number',
+      flex: 1
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1
+    },
+    {
+      field: 'address',
+      headerName: 'Address',
+      flex: 1
+    },
+    {
+      field: 'city',
+      headerName: 'City',
+      flex: 1
+    },
+    {
+      field: 'zipCode',
+      headerName: 'Zip Code',
+      flex: 1
+    }
+  ]
+  return (
+    <Box m='20px' p='20px'>
+      <Header
+        title='contacts'
+        subtitle='List of contacts for future reference.'
+      />
+      <Box
+        m='40px 0 0 0'
+        height='75vh'
+        sx={{
+          '& .MuiDataGrid-root': {
+            border: 'none'
+          },
+          '& .MuiDataGrid-cell': {
+            borderBottom: 'none'
+          },
+          '& .name-column--cell': {
+            color: colors.greenAccent[300]
+          },
+          '& .MuiDataGrid-columnHeaders': {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: 'none'
+          },
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: colors.primary[400]
+          },
+          '& .MuiDataGrid-footerContainer': {
+            borderTop: 'none',
+            backgroundColor: colors.blueAccent[700]
+          },
+          '& .MuiCheckbox-root': {
+            color: `${colors.greenAccent[200]} !important`
+          },
+          '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+            color: `${colors.grey[100]} !important`
+          }
+        }}
+      >
+        <DataGrid
+          rows={mockDataContacts}
+          columns={cols}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
+    </Box>
+  )
 }
 
-export default Contacts;
+export default Contacts
