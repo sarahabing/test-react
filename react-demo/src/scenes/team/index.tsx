bccc496fa55a104cc35908f74e0020a141c11641
sarahabing <abingsa1@gmail.com>
2023-02-16 17:06:13 -0700

    table styles

--- react-demo/src/scenes/team/index.tsx
+++ react-demo/src/scenes/team/index.tsx
@@ -63,9 +63,37 @@ function Team(): JSX.Element {
     }
   ]
   return (
-    <Box m='20px'>
+    <Box m='20px' p="20px">
       <Header title='team' subtitle='Managing the team members.' />
-      <Box m='40px 0 0 0' height='75vh'>
+      <Box
+        m='40px 0 0 0'
+        height='75vh'
+        sx={{
+          '& .MuiDataGrid-root': {
+            border: 'none'
+          },
+          '& .MuiDataGrid-cell': {
+            borderBottom: 'none'
+          },
+          '& .name-column--cell': {
+            color: colors.greenAccent[300]
+          },
+          '& .MuiDataGrid-columnHeaders': {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: 'none'
+          },
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: colors.primary[400]
+          },
+          '& .MuiDataGrid-footerContainer': {
+            borderTop: 'none',
+            backgroundColor: colors.blueAccent[700]
+          },
+          '& .MuiCheckbox-root': {
+            color: `${colors.greenAccent[200]} !important`
+          }
+        }}
+      >
         <DataGrid rows={mockDataTeam} columns={cols} />
       </Box>
     </Box>
